f27d963ef5fd5ef0cd20dfeef9bcf5dbffad11ea
ameen <ameenemails@gmail.com>
2023-09-17 10:43:08 +0300

    create home repo method

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,5 +3,5 @@ import 'package:news_app/core/errors/failure.dart';
 import 'package:news_app/features/home/data/models/artical_model/artical_model.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<ArticalModel>>> getNews();
+ Future<Either<Failure, List<ArticalModel>>> getNews ();
 }
