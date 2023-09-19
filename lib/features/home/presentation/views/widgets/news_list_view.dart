7d4496cb14fb886e5b2174948976eb57de030d65
ameen <ameenemails@gmail.com>
2023-09-19 10:04:43 +0300

    add bloc builder

--- lib/features/home/presentation/views/widgets/news_list_view.dart
+++ lib/features/home/presentation/views/widgets/news_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/core/widgets/custom_error_widget.dart';
+import 'package:news_app/features/home/presentation/manager/fetch_news_cubit/fetch_news_cubit.dart';
 import 'news_tile.dart';
 
 class NewsListView extends StatelessWidget {
@@ -7,18 +9,30 @@ class NewsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (context, index) {
-          return const Padding(
-            padding: EdgeInsets.only(
-              bottom: 22,
+    return BlocBuilder<FetchNewsCubit, FetchNewsState>(
+      builder: (context, state) {
+        if (state is FetchNewsSuccess) {
+          return SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 22,
+                  ),
+                  child: NewsTile(),
+                );
+              },
+              childCount: 10,
             ),
-            child: NewsTile(),
           );
-        },
-        childCount: 10,
-      ),
+        } else if (state is FetchNewsfailure) {
+          return CustomErrorWidget(
+            errMessage: state.errMessage,
+          );
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
