feafcbdd639ad7b472f4596b90a4f62f4d86996b
ameen <amennzullaq>
2024-09-06 10:38:34 +0300

    add my widget 3

---
+++ lib/new_widget3.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyWidget3 extends StatelessWidget {
+  const MyWidget3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
